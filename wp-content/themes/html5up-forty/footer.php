c0478cc138ace544b7fa89db057f2c52a1a7346d
Lino Espinoza <woztheman@gmail.com>
2018-04-04 19:51:59 -0500

    Se actualiza los archivos de la sesion9

--- wp-content/themes/html5up-forty/footer.php
+++ wp-content/themes/html5up-forty/footer.php
@@ -51,6 +51,17 @@
 
 				<!-- Footer -->
 					<footer id="footer">
+						<div id="widgetized-area">
+							<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('widgetized-area')) : else : ?>
+
+							<div class="pre-widget">
+								<p><strong>Widgetized Area</strong></p>
+								<p>This panel is active and ready for you to add some widgets via the WP Admin</p>
+							</div>
+
+							<?php endif; ?>
+						</div>
+
 						<div class="inner">
 							<ul class="icons">
 								<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
--- wp-content/themes/html5up-forty/functions.php
+++ wp-content/themes/html5up-forty/functions.php
@@ -58,4 +58,19 @@ add_filter('script_loader_tag', 'add_noscript_filter', 10, 3);
 
 
 
+if (function_exists('register_sidebar')) {
+	register_sidebar(array(
+		'name' => 'Widgetized Area',
+		'id'   => 'widgetized-area',
+		'description'   => 'This is a widgetized area.',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h4>',
+		'after_title'   => '</h4>'
+	));
+
+}
+
+
+
 ?>
